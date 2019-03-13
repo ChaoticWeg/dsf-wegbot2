604ba109cf61e0d1fb615bec4ae70abb27515f3e
ChaoticWeg <shawn@chaoticweg.cc>
2019-03-13 17:40:58 -0500

    complete coverage
    switched to unbuilt TS for testing

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,28 @@
-import Wegbot from '../lib/index';
+import Wegbot from '../src/index';
 
 test('creates the instance', () => {
     expect(Wegbot.instance).not.toBeNull();
     expect(Wegbot.instance).not.toBeUndefined();
     expect(Wegbot.instance).not.toBeFalsy();
+});
+
+test('logs in and out successfully', async () => {
+    expect.assertions(8);
+
+    let loginPromise : Promise<string> = Wegbot.start();
+
+    expect(loginPromise).not.toBeFalsy();
+    expect(loginPromise).toHaveProperty("then");
+    expect(typeof loginPromise.then).toEqual("function");
+
+    let token : string = await loginPromise;
+
+    expect(token).not.toBeFalsy();
+    expect(token).toEqual(Wegbot.token);
+
+    let logoutPromise : Promise<any> = Wegbot.logout();
+
+    expect(logoutPromise).not.toBeFalsy();
+    expect(logoutPromise).toHaveProperty("then");
+    expect(typeof logoutPromise.then).toEqual("function");
 });
