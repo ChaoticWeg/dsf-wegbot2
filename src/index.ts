9f155d221af47ce24b2c69945fe551fbb1e9bfab
ChaoticWeg <shawn@chaoticweg.cc>
2019-03-13 17:40:29 -0500

    add token and logout methods

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,32 @@ import Discord from "discord.js";
 
 class Wegbot {
     private _instance : Discord.Client;
+    private _token : string;
 
     constructor() {
         this._instance = new Discord.Client();
-
-        
+        this._token = require('../creds.json').token;
     }
 
-    get instance() : Discord.Client {
+    public get instance() : Discord.Client {
         return this._instance;
     }
-}
+
+    public get token() : string {
+        return this._token;
+    }
+
+    public start() : Promise<string> {
+        return this._instance.login(this._token);
+    }
+
+    public logout() : Promise<any> {
+        return this._instance.destroy();
+    }
+};
+
+const _instance = new Wegbot();
 
 export default (() => {
-    return new Wegbot();
+    return _instance;
 })();
