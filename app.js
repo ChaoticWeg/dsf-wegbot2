7a9dc516e43c92ddaa56baa92f8a053459deefc7
ChaoticWeg <shawn@chaoticweg.cc>
2019-03-14 23:08:48 -0500

    events: register events and apply to Client

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 import Wegbot from './lib';
+import { EventRegistry } from "./lib/events";
+
+EventRegistry.instance.applyAll(Wegbot.instance);
 Wegbot.start();
--- src/events/index.ts
+++ src/events/index.ts
@@ -1,1 +1,17 @@
+import { WegbotEvent } from "./baseEvent";
+import { ReadyEvent } from "./ready";
+import { EchoMessageEvent } from "./message";
+
+export default (() => {
+    
+    // instantiate each type of event - they will register themselves
+    const events: Array<WegbotEvent> = [
+        new ReadyEvent(),
+        new EchoMessageEvent()
+    ];
+
+    return events;
+
+})();
+
 export { EventRegistry } from "./registry";
