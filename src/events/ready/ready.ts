3db5a17208965821742018fdae0d1362f8122f5e
ChaoticWeg <shawn@chaoticweg.cc>
2019-03-17 00:48:07 -0500

    lint: swap single quotes for doubles

--- src/events/ready/ready.ts
+++ src/events/ready/ready.ts
@@ -2,10 +2,10 @@ import { WegbotEvent } from "../baseEvent";
 
 export class ReadyEvent extends WegbotEvent {
     public constructor() {
-        super('ready');
+        super("ready");
     }
 
     public onTriggered() : void {
-        console.log('bot is ready');
+        console.log("bot is ready");
     }
 }
